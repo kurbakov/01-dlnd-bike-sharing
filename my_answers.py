16c7b559019c472514b8d8ba8aa5aad78a9d4564
kurbakov <kurbakov.d@gmail.com>
2019-11-10 15:27:37 +0100

    implement forward function and add sigmoid

--- my_answers.py
+++ my_answers.py
@@ -16,19 +16,7 @@ class NeuralNetwork(object):
                                        (self.hidden_nodes, self.output_nodes))
         self.lr = learning_rate
         
-        #### TODO: Set self.activation_function to your implemented sigmoid function ####
-        #
-        # Note: in Python, you can define a function with a lambda expression,
-        # as shown below.
-        self.activation_function = lambda x : 0  # Replace 0 with your sigmoid calculation.
-        
-        ### If the lambda code above is not something you're familiar with,
-        # You can uncomment out the following three lines and put your 
-        # implementation there instead.
-        #
-        #def sigmoid(x):
-        #    return 0  # Replace 0 with your sigmoid calculation here
-        #self.activation_function = sigmoid
+        self.activation_function = lambda x : 1/(1+np.exp(-x))
                     
 
     def train(self, features, targets):
@@ -59,17 +47,14 @@ class NeuralNetwork(object):
             Arguments
             ---------
             X: features batch
-
         '''
         #### Implement the forward pass here ####
         ### Forward pass ###
-        # TODO: Hidden layer - Replace these values with your calculations.
-        hidden_inputs = None # signals into hidden layer
-        hidden_outputs = None # signals from hidden layer
+        hidden_inputs = np.dot(X, self.weights_input_to_hidden)
+        hidden_outputs = self.activation_function(hidden_outputs)
 
-        # TODO: Output layer - Replace these values with your calculations.
-        final_inputs = None # signals into final output layer
-        final_outputs = None # signals from final output layer
+        final_inputs = np.dot(hidden_oututs, self.weights_hidden_to_output)
+        final_outputs = final_inputs
         
         return final_outputs, hidden_outputs
 
